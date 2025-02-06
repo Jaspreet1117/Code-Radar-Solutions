74b1c323cef9fc77e2328e48608c11373413c72f
Jaspreet1117 <sekhonjaspreet2006@gmail.com>
2025-02-06 22:56:31 +0530

    Solution to Toggle nth Bit of a Number

--- Toggle nth Bit of a Number@@67882a731b4392d8fc1577ed/code.c
+++ Toggle nth Bit of a Number@@67882a731b4392d8fc1577ed/code.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-
-char* welcome() {
-    return "Welcome to Code Radar!";
-}
-
 int main() {
     int n1,n2,result;
-    scanf("%d%d",&n1,n2);
+    scanf("%d%d",&n1,&n2);
     result=n1^(1<<n2);
     printf("%d",result);
     return 0;
