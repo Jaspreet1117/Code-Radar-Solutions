aa74846560daaf4e853c94972d6e7b0ec58ecfb2
Jaspreet1117 <sekhonjaspreet2006@gmail.com>
2025-02-09 22:11:32 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -6,8 +6,8 @@ int main(){
 
     if(ch=='+'){printf("%d",num1+num2);}
     else if(ch=='-'){printf("%d",num1-num2);}
-   /* else if(ch=='*'){printf("%d",num1*num2);}
-    else(ch=='/'){if(num2!=0){printf("%d",num1/num2);}
+   else if(ch=='*'){printf("%d",num1*num2);}
+   /* else(ch=='/'){if(num2!=0){printf("%d",num1/num2);}
                  else {printf("error");}}*/
     
     return 0;
