ebaf81f3b270bf92d52426fb8fa3ce428dce1972
Jaspreet1117 <sekhonjaspreet2006@gmail.com>
2025-03-31 21:59:18 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

--- Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -19,9 +19,9 @@ void trackPlayerRanks(int *ranked,int n,int *player,int m,int *result){
             else{
                 break;
             }
-
-        }
         result[check]=count+1;
+        }
+        
         check+=1;
 
     }
